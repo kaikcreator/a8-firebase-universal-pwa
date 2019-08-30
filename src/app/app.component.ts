01656927be03884b5f00f849dab67bc54758beb3
Enrique Oriol <enrique.oriol@dynatrace.com>
2019-08-30 13:14:54 +0200

    Added PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwPush, SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'eoriol-v8';
+public ngOnInit(): void {
+        if (this.swUpdate.isEnabled) {
+            this.swUpdate.available.subscribe((evt) => {
+                console.log('service worker updated');
+                this.swUpdate.activateUpdate()
+                .then(result => console.log('SW updated!'))
+                .catch(err => console.log('Error updating the SW, ', err));
+            });
+
+            this.swUpdate.checkForUpdate().then(() => {
+                // noop
+            }).catch((err) => {
+                console.error('error when checking for update', err);
+            });
+        }}
+
+  constructor(private swUpdate: SwUpdate, public swPush: SwPush) {}
 }
--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -4,12 +4,14 @@ import { ServerModule } from '@angular/platform-server';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+//import { NgtPwaMockModule } from '@ng-toolkit/pwa';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
     ModuleMapLoaderModule,
+//    NgtPwaMockModule,
   ],
   bootstrap: [AppComponent],
 })
