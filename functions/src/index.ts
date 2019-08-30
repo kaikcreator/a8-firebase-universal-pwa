b8053006d0f2ae52f37730cec218c352b8b192b0
Enrique Oriol <enrique.oriol@dynatrace.com>
2019-08-30 17:57:03 +0200

    debugging firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,8 @@
 import * as functions from 'firebase-functions';
 
 export const universal = functions.https.onRequest((request, response) => {
-    require(`${process.cwd()}/dist/server`).app(request, response);
+  //require(`${process.cwd()}/dist/server`).app(request, response);
+  const server = require(`${process.cwd()}/dist/server`);
+  console.log("server JSON is: ", JSON.stringify(server));
+    server.app(request, response);
   });
